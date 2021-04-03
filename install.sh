ef0e24ed0c48b50c731f1c9e7a14cf709c647303
Wang Liang <wangliangzz@inspur.com>
2021-04-03 11:01:39 +0800

    support youcompleteme at ubuntu20.04

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 # yum install -y vim tmux git ctags cscope
-sudo apt-get install -y vim tmux git ctags cscope cmake python3-dev build-essential bison flex clang python  pkg-config libfmt-dev libspdlog-dev nlohmann-json3-dev libgrpc++-dev protobuf-compiler-grpc libssl-dev bear libelf-dev 
+sudo apt-get install -y vim tmux git ctags cscope cmake python3-dev build-essential bison flex clang python  pkg-config libfmt-dev libspdlog-dev nlohmann-json3-dev libgrpc++-dev protobuf-compiler-grpc libssl-dev bear libelf-dev llvm clang libclang-dev libboost-all-dev
 cp -rf vimrc ~/.vimrc
 sudo cp tmux.conf /etc/tmux.conf
 mkdir -p ~/.vim/autoload/
 cp plug.vim ~/.vim/autoload/
 
 echo "Please run :PlugInstall in vim"
-echo "Please use make menuconfig & bear make to build kernel source"
+echo "Please cd ~/.vim/plugged/youcompleteme/ && ./install.py --clang-completer"
+echo "Please use make menuconfig & bear make -j8 to build kernel source"
+
